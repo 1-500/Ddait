afc61b93d22a57e0b6e038cfee9e0f087692be1f
nara04040 <nara04040@naver.com>
2024-08-12 11:56:42 +0900

    Build: 폴더세팅

---
+++ src/components/HeaderComponents.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const HeaderComponents = () => {
+  return (
+    <View>
+      <Text>HeaderComponents</Text>
+    </View>
+  )
+}
+
+export default HeaderComponents
+
+const styles = StyleSheet.create({})
---
+++ src/hooks/hook.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const hook = () => {
+  return (
+    <View>
+      <Text>hook</Text>
+    </View>
+  )
+}
+
+export default hook
+
+const styles = StyleSheet.create({})
