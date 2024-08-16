4f3f3e781abd8ca2ecdae8ca2f568b74bfbca174
nara04040 <nara04040@naver.com>
2024-08-16 16:00:23 +0900

    :building_construction: Structure: 폴더구조 설정

---
+++ src/pages/competition/Competition.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Competition = () => {
+  return (
+    <View>
+      <Text>Competition</Text>
+    </View>
+  )
+}
+
+export default Competition
+
+const styles = StyleSheet.create({})
---
+++ src/pages/diary/workoutDiary/workoutDiary.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const workoutDiary = () => {
+  return (
+    <View>
+      <Text>workoutDiary</Text>
+    </View>
+  )
+}
+
+export default workoutDiary
+
+const styles = StyleSheet.create({})
