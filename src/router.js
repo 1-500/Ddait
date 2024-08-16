145015626f379cf5fc9d759add571fe7aca08048
nara04040 <nara04040@naver.com>
2024-08-16 16:26:07 +0900

    :building_construction: Build: router 세팅

--- src/router.js
+++ src/router.js
@@ -1,1 +1,13 @@
-// 라우팅
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createStackNavigator } from '@react-navigation/stack'
+
+
+const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator()
+
+const Router = () => {
+  return // 라우팅
+}
+
+export default Router
