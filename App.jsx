ea64c6136202cbc6c7e218b5d9293f6cf2bb17b8
nara04040 <nara04040@naver.com>
2024-08-19 20:26:52 +0900

    ISSUE-32 :art: Style: tailwind config 설정

--- App.jsx
+++ App.jsx
@@ -8,8 +8,14 @@ import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 function App() {
   return (
     <GluestackUIProvider>
-      <SafeAreaView className="flex-1 items-center justify-center bg-error-50">
-        <Text className="text-success-500 text-xl mb-5">hello world</Text>
+      <SafeAreaView className="flex-1 items-center justify-center bg-background-dark">
+        <Text className="text-xl mb-5 color-primary-300">hello world</Text>
+        <Text className="text-xl mb-5 color-primary-500">hello world</Text>
+        <Text className="text-xl mb-5 color-primary-800">hello world</Text>
+        <Text className="text-xl mb-5 color-secondary-300">hello world</Text>
+        <Text className="text-xl mb-5 color-secondary-500">hello world</Text>
+        <Text className="text-xl mb-5 color-secondary-800">hello world</Text>
+        <Text className="text-xl mb-5 color-error-400">hello world</Text>
       </SafeAreaView>
     </GluestackUIProvider>
   );
