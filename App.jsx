0e8cf5e0eebd538f211587e4f2b9d185a7e2dd04
nara04040 <nara04040@naver.com>
2024-08-19 20:07:42 +0900

    ISSUE-32 :bug: fix: gluestack module 에러 수정

--- App.jsx
+++ App.jsx
@@ -1,12 +1,18 @@
+import './global.css';
+
 import React from 'react';
-import "@/global.css";
-import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
-import { Text, View } from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
+
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 
 function App() {
   return (
-    <View>
-      <Text>Test</Text>
-    </View>
+    <GluestackUIProvider>
+      <SafeAreaView className="flex-1 items-center justify-center bg-error-50">
+        <Text className="text-success-500 text-xl mb-5">hello world</Text>
+      </SafeAreaView>
+    </GluestackUIProvider>
   );
 }
+
+export default App;
