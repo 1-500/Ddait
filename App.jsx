2b9beaf3b813a00d25be4e53685119901dfaaad0
ChangHyeon Lee <canghaun@gmail.com>
2024-08-20 23:03:16 +0900

    ISSUE-33 :sparkles: Feat: 바텀탭 컴포넌트 구현 및 라우터 적용

--- App.jsx
+++ App.jsx
@@ -1,22 +1,27 @@
 import './global.css';
 
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
 
 import { GluestackUIProvider } from './components/ui/gluestack-ui-provider';
+import Router from './src/router';
 
 function App() {
   return (
     <GluestackUIProvider>
-      <SafeAreaView className="flex-1 items-center justify-center bg-background-dark">
-        <Text className="text-xl mb-5 color-primary-300">hello world</Text>
-        <Text className="text-xl mb-5 color-primary-500">hello world</Text>
-        <Text className="text-xl mb-5 color-primary-800">hello world</Text>
-        <Text className="text-xl mb-5 color-secondary-300">hello world</Text>
-        <Text className="text-xl mb-5 color-secondary-500">hello world</Text>
-        <Text className="text-xl mb-5 color-secondary-800">hello world</Text>
-        <Text className="text-xl mb-5 color-error-400">hello world</Text>
-      </SafeAreaView>
+      <NavigationContainer>
+        {/* <SafeAreaView className="flex-1 items-center justify-center bg-background-dark">
+          <Text className="text-xl mb-5 color-primary-300">hello world</Text>
+          <Text className="text-xl mb-5 color-primary-500">hello world</Text>
+          <Text className="text-xl mb-5 color-primary-800">hello world</Text>
+          <Text className="text-xl mb-5 color-secondary-300">hello world</Text>
+          <Text className="text-xl mb-5 color-secondary-500">hello world</Text>
+          <Text className="text-xl mb-5 color-secondary-800">hello world</Text>
+          <Text className="text-xl mb-5 color-error-400">hello world</Text>
+        </SafeAreaView> */}
+        <Router />
+      </NavigationContainer>
     </GluestackUIProvider>
   );
 }
