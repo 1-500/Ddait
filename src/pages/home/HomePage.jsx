397454f0c60164dcd90176f8d2a9ef93d94489df
nara04040 <nara04040@naver.com>
2024-08-22 01:20:40 +0900

    ISSUE-119 :sparkles: Feat: CustomInput 개발

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,14 +1,52 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import DefaultInput from '../../components/CustomInput';
+import { LAYOUT_PADDING } from '../../constants/space';
+
 const HomePage = () => {
   return (
-    <View>
-      <Text>HomePage</Text>
+    <View style={[styles.container, LAYOUT_PADDING]}>
+      <Text>input</Text>
+      <DefaultInput
+        theme="primary"
+        size="medium"
+        value=""
+        placeholder="플레이스홀더야"
+        secureTextEntry={false}
+        isPassword={true}
+        isError={true}
+      />
+      <Text>input</Text>
+      <DefaultInput
+        theme="user"
+        size="medium"
+        value=""
+        placeholder="플레이스홀더야"
+        secureTextEntry={false}
+        isPassword={true}
+        isError={true}
+      />
+      <Text>input</Text>
+      <DefaultInput
+        theme="search"
+        size="medium"
+        value=""
+        placeholder="플레이스홀더야"
+        secureTextEntry={false}
+        isPassword={true}
+        isError={true}
+      />
     </View>
   );
 };
 
 export default HomePage;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
