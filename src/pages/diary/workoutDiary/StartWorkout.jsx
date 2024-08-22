32c40f011edeacbf17ccc83446b400924b054d23
nara04040 <nara04040@naver.com>
2024-08-22 13:54:14 +0900

    ISSUE-71 :sparkles: Feat: StartWorkout page 생성

---
+++ src/pages/diary/workoutDiary/StartWorkout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const StartWorkout = () => {
+  return (
+    <View>
+      <Text>StartWorkout</Text>
+    </View>
+  );
+};
+
+export default StartWorkout;
+
+const styles = StyleSheet.create({});
