448e3e1935ee2166c708a1796ed7b22eff5262f7
nara04040 <nara04040@naver.com>
2024-08-22 11:05:03 +0900

    ISSUE-134 :lipstick: UI: Card Component 스타일 개선

--- src/components/CustomCardComponent.jsx
+++ src/components/CustomCardComponent.jsx
@@ -1,42 +1,56 @@
 import React from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
 
-import { BACKGROUND_COLORS } from '../constants/colors';
+import { BACKGROUND_COLORS, TAG_COLORS, TEXT_COLORS } from '../constants/colors';
+import { FONT_SIZES } from '../constants/font';
 
 /**
  * CustomCard Component
  *
  * @param {{
+ *   theme: 'findCompetition' | 'competitionRoom' | 'createCompetition',
  *   title: string,
  *   background: string,
- *   tags: Array<{ label: string, color: string }>,
- *   additionalInfo: React.ReactNode,
- *   footer: React.ReactNode,
+ *   tags: Array<{ label: string, category: string }>,
+ *   additionalInfo: string,
+ *   footer: string,
  * }} props
  */
 
 const CustomCard = ({ title, tags, additionalInfo, footer }) => {
   return (
     <View style={styles.card}>
-      <View style={styles.header}>
-        <Text style={styles.title}>{title}</Text>
-        {additionalInfo && <View style={styles.additionalInfo}>{additionalInfo}</View>}
-      </View>
-      <View style={styles.tagsContainer}>
-        {tags.map((tag, index) => (
-          <View key={index} style={[styles.tag, { backgroundColor: tag.color }]}>
-            <Text style={styles.tagText}>{tag.label}</Text>
+      <View>
+        <View style={styles.header}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+        <View style={styles.tagsContainer}>
+          {tags.map((tag, index) => (
+            <View key={index} style={[styles.tag, { backgroundColor: TAG_COLORS[tag.category] }]}>
+              <Text style={styles.tagText}>{tag.label}</Text>
+            </View>
+          ))}
+        </View>
+        {footer && (
+          <View style={styles.footer}>
+            <Text style={styles.footerText}>{footer}</Text>
           </View>
-        ))}
+        )}
       </View>
-      {footer && <View style={styles.footer}>{footer}</View>}
+      <View style={styles.additionalInfo}>
+        <Image source={require('../assets/images/sun.png')} />
+        <Text style={styles.additionalInfoText}>{additionalInfo}</Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   card: {
+    flexDirection: 'row',
     backgroundColor: BACKGROUND_COLORS.greyDark,
+    justifyContent: 'space-between',
+    width: '100%',
     borderRadius: 10,
     padding: 16,
     marginBottom: 16,
@@ -50,11 +64,17 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 16,
     fontWeight: 'bold',
-    color: '#FFF',
+    color: TEXT_COLORS.primary,
   },
   additionalInfo: {
     flexDirection: 'row',
+    justifyContent: 'center',
     alignItems: 'center',
+  },
+  additionalInfoText: {
+    fontSize: FONT_SIZES.xs,
+    color: TEXT_COLORS.primary,
+    marginLeft: 4,
   },
   tagsContainer: {
     flexDirection: 'row',
@@ -67,13 +87,18 @@ const styles = StyleSheet.create({
     marginRight: 8,
   },
   tagText: {
-    color: '#FFF',
-    fontSize: 12,
+    color: TEXT_COLORS.primary,
+    marginLeft: 4,
+    fontSize: FONT_SIZES.xs,
   },
   footer: {
     marginTop: 8,
     flexDirection: 'row',
     justifyContent: 'space-between',
+  },
+  footerText: {
+    fontSize: FONT_SIZES.xs,
+    color: TEXT_COLORS.primary,
   },
 });
 
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 import CustomCard from '../../components/CustomCardComponent';
-import { TAG_COLORS, TEXT_COLORS } from '../../constants/colors';
+import { LAYOUT_PADDING } from '../../constants/space';
 
 const HomePage = () => {
   return (
@@ -12,15 +12,11 @@ const HomePage = () => {
         <CustomCard
           title="스쿼트 챌린지 들어오세요!"
           tags={[
-            { label: '헬스', color: TAG_COLORS.weight },
-            { label: '스쿼트', color: TAG_COLORS.runnging },
+            { label: '헬스', category: 'weight' },
+            { label: '스쿼트', category: 'runnging' },
           ]}
-          additionalInfo={
-            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-              <Text style={{ color: TEXT_COLORS.primary, marginLeft: 4 }}>3/10</Text>
-            </View>
-          }
-          footer={<Text style={{ color: TEXT_COLORS.primary }}>23.08.01 ~ 23.09.01</Text>}
+          additionalInfo={'3/10'}
+          footer={'23.08.01 ~ 23.09.01'}
         />
       </View>
     </SafeAreaView>
@@ -34,5 +30,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    ...LAYOUT_PADDING,
   },
 });
