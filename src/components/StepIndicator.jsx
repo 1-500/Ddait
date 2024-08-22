e71e02f818b3068d5645304b7f3bf694395bdf7f
aong13 <oaoa13@pukyong.ac.kr>
2024-08-22 20:06:39 +0900

    ISSUE-39 :lipstick: UI: stepIndicator 애니메이션 적용

--- src/components/StepIndicator.jsx
+++ src/components/StepIndicator.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { Animated, Dimensions, StyleSheet, View } from 'react-native';
+
+const { width } = Dimensions.get('window');
 
 const StepIndicator = ({ currentStep, steps }) => {
-  const stepIndicators = Array.from({ length: steps }, (_, index) => (
-    <View key={index} style={[styles.step, index === currentStep - 1 ? styles.activeStep : styles.inactiveStep]} />
-  ));
-  return <View style={styles.container}>{stepIndicators}</View>;
+  const animatedValue = useRef(new Animated.Value(0)).current;
+
+  const stepWidth = (width - 40 - (steps - 2) * 8) / steps;
+  const stepGap = 8;
+
+  useEffect(() => {
+    Animated.timing(animatedValue, {
+      toValue: currentStep - 1,
+      useNativeDriver: false,
+    }).start();
+  }, [animatedValue, currentStep]);
+
+  // 각 단계 간 위치 계산
+  const translateX = animatedValue.interpolate({
+    inputRange: Array.from({ length: steps }, (_, index) => index),
+    outputRange: Array.from({ length: steps }, (_, index) => index * (stepWidth + stepGap)),
+  });
+
+  const stepIndicators = Array.from({ length: steps }, (_, index) => <View key={index} style={styles.step} />);
+
+  return (
+    <View style={styles.container}>
+      {stepIndicators}
+      <Animated.View style={[styles.activeBar, { transform: [{ translateX }] }, { width: stepWidth }]} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -13,23 +37,25 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-    gap: 10,
+    position: 'relative',
+    gap: 8,
   },
   step: {
     height: 7,
     borderRadius: 20,
     flex: 1,
+    backgroundColor: '#000',
   },
-  activeStep: {
+  activeBar: {
+    position: 'absolute',
+    height: 7,
+    borderRadius: 20,
     backgroundColor: '#5d5dfc',
     shadowColor: 'rgba(255, 255, 255, 0.25)',
     shadowOffset: { width: 0, height: 0 },
     shadowRadius: 4,
     shadowOpacity: 1,
     elevation: 4,
-  },
-  inactiveStep: {
-    backgroundColor: '#000',
   },
 });
 
