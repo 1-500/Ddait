a4600bf2c58f3f90771fb66b0863671b765a5301
ChangHyeon Lee <canghaun@gmail.com>
2024-08-22 13:41:52 +0900

    ISSUE-31 :sparkles: Feat: 헤더 컴포넌트 구현

--- src/components/HeaderComponents.jsx
+++ src/components/HeaderComponents.jsx
@@ -1,14 +1,127 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
 
-const HeaderComponents = () => {
+import { COLORS } from '../constants/colors';
+import { FONT_WEIGHTS, HEADER_FONT_SIZES } from '../constants/font';
+import { SPACING } from '../constants/space';
+
+/**
+ *
+ * @param {{
+ * icon: 'none' | 'btn' | 'search' | 'setting' | 'timer' | 'scrap_on' | 'scrap_off' | 'menu' | 'date';
+ * title: string
+ * onRightBtnPress: () => void;
+ * onDatePress: () => void;
+ * }} param0
+ */
+
+const HeaderComponents = ({ icon = 'none', title = '', onRightBtnPress = () => {}, onDatePress = () => {} }) => {
+  const getRightBtn = () => {
+    switch (icon) {
+      case 'btn':
+        return (
+          <TouchableOpacity style={styles.btnWrapper} onPress={onRightBtnPress} activeOpacity={0.6}>
+            <Text style={styles.submitBtnTitle}>저장</Text>
+          </TouchableOpacity>
+        );
+      case 'search':
+        return (
+          <TouchableOpacity style={styles.btnWrapper} onPress={onRightBtnPress} activeOpacity={0.6}>
+            <Ionicons name="search-sharp" size={24} color={COLORS.white} />
+          </TouchableOpacity>
+        );
+      case 'setting':
+        return (
+          <TouchableOpacity style={styles.btnWrapper} onPress={onRightBtnPress} activeOpacity={0.6}>
+            <SimpleLineIcons name="settings" size={24} color={COLORS.white} />
+          </TouchableOpacity>
+        );
+      case 'timer':
+        return (
+          <TouchableOpacity style={styles.btnWrapper} onPress={onRightBtnPress} activeOpacity={0.6}>
+            <MaterialCommunityIcons name="timer-outline" size={24} color={COLORS.primary} />
+          </TouchableOpacity>
+        );
+      case 'scrap_on':
+        return (
+          <TouchableOpacity style={styles.btnWrapper} onPress={onRightBtnPress} activeOpacity={0.6}>
+            <MaterialCommunityIcons name="bookmark" size={24} color={COLORS.primary} />
+          </TouchableOpacity>
+        );
+      case 'scrap_off':
+        return (
+          <TouchableOpacity style={styles.btnWrapper} onPress={onRightBtnPress} activeOpacity={0.6}>
+            <MaterialCommunityIcons name="bookmark-outline" size={24} color={COLORS.white} />
+          </TouchableOpacity>
+        );
+      case 'menu':
+        return (
+          <TouchableOpacity style={styles.btnWrapper} onPress={onRightBtnPress} activeOpacity={0.6}>
+            <MaterialCommunityIcons name="dots-vertical" size={24} color={COLORS.white} />
+          </TouchableOpacity>
+        );
+      default:
+        return <View style={styles.btnWrapper} />;
+    }
+  };
+
   return (
     <View>
-      <Text>HeaderComponents</Text>
+      {icon !== 'date' ? (
+        <View style={styles.headerContainer}>
+          <TouchableOpacity style={styles.btnWrapper} activeOpacity={0.6}>
+            <FontAwesome name="angle-left" size={24} color={COLORS.white} />
+          </TouchableOpacity>
+          <Text style={styles.titleText}>{title}</Text>
+          {getRightBtn()}
+        </View>
+      ) : (
+        <View style={[styles.headerContainer, { justifyContent: 'center' }]}>
+          <TouchableOpacity style={styles.dateWrapper} onPress={onDatePress} activeOpacity={0.6}>
+            <Text style={styles.titleText}>{title}</Text>
+            <FontAwesome name="angle-down" size={24} color={COLORS.white} />
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 };
 
 export default HeaderComponents;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: SPACING.xs,
+    borderBottomWidth: 1,
+    borderBottomColor: COLORS.primary,
+  },
+  btnWrapper: {
+    width: 44,
+    height: 44,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  titleText: {
+    fontSize: HEADER_FONT_SIZES.sm,
+    fontWeight: FONT_WEIGHTS.bold,
+    lineHeight: 44,
+    color: COLORS.white,
+  },
+  submitBtnTitle: {
+    fontSize: 16,
+    fontWeight: FONT_WEIGHTS.semiBold,
+    color: COLORS.primary,
+  },
+  dateWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: SPACING.xs,
+  },
+});
