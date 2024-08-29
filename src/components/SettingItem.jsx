88ac3d60ab609f8f21f1c4cf03f5d98f35862f64
InKyungWoo <chelsea1201@naver.com>
2024-08-29 17:06:35 +0900

    ISSUE-181 :sparkles: Feat: 설정 항목 아이템 컴포넌트 구현

---
+++ src/components/SettingItem.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+
+import { COLORS } from '../constants/colors';
+import { FONTS } from '../constants/font';
+import Toggle from './Toggle';
+
+const SettingItem = ({ title, description, leftBtn, rightBtn, onPress, onToggle, isToggled, isRadioActive }) => {
+  const renderLeftBtn = () => {
+    if (leftBtn === 'radio') {
+      return (
+        <Fontisto name={isRadioActive ? 'radio-btn-active' : 'radio-btn-passive'} size={20} color={COLORS.primary} />
+      );
+    }
+  };
+
+  const renderRightBtn = () => {
+    switch (rightBtn) {
+      case 'arrow':
+        return <FontAwesome name="angle-right" size={24} color={COLORS.white} />;
+      case 'toggle':
+        return <Toggle isOn={isToggled} onToggle={onToggle} />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <TouchableOpacity onPress={onPress} activeOpacity={0.6}>
+      <View style={styles.container}>
+        {renderLeftBtn()}
+        <View style={{ gap: 12 }}>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.description}>{description}</Text>
+        </View>
+        {renderRightBtn()}
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default SettingItem;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderBottomWidth: 1,
+    borderColor: COLORS.darkGrey,
+    padding: 20,
+  },
+  title: {
+    color: COLORS.white,
+    fontSize: 16,
+    fontFamily: FONTS.PRETENDARD[500],
+  },
+  description: {
+    color: COLORS.white,
+    fontFamily: FONTS.PRETENDARD[300],
+  },
+});
