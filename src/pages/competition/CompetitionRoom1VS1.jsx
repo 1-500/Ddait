a5cb2fed0d96c724f6f8a5e984f6df7d379a7042
ChangHyeon Lee <canghaun@gmail.com>
2024-09-03 18:09:31 +0900

    ISSUE-178 :sparkles: Feat: 1:1 경쟁방 페이지 API 연결

--- src/pages/competition/CompetitionRoom1VS1.jsx
+++ src/pages/competition/CompetitionRoom1VS1.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import {
   Alert,
   Dimensions,
+  FlatList,
   Image,
   SafeAreaView,
   ScrollView,
@@ -23,83 +24,16 @@ import { LAYOUT_PADDING, SPACING } from '../../constants/space';
 
 const { width } = Dimensions.get('window');
 
-/* eslint-disable */
+const dummyProfile = require('../../assets/images/profile.png');
 
-const dummy_data = {
-  room_info: {
-    title: '나라랑 따잇이랑',
-    max_members: 2,
-    competition_type: '웨이트트레이닝',
-    competition_theme: '경쟁 세부 테마',
-    is_public: false,
-    start_date: new Date(2024, 7, 1), //2024-08-01
-    end_date: new Date(2024, 7, 31), //2024-08-31
-  },
-  record: [
-    {
-      id: 'competition_record_uuid1',
-      member_id: 'member_uuid1',
-      competition_room_id: 'competition_room_uuid1',
-      rank: null,
-      total_score: 19.79,
-      created_at: '2024-08-26T18:17:51.368969+00:00',
-      score_detail: [
-        {
-          name: '데드리프트',
-          score: 0,
-        },
-        {
-          name: '벤치프레스',
-          score: 2.51,
-        },
-        {
-          name: '스쿼트',
-          score: 17.28,
-        },
-      ],
-    },
-    {
-      id: 'competition_record_uuid2',
-      member_id: 'member_uuid2',
-      competition_room_id: 'competition_room_uuid1',
-      rank: null,
-      total_score: 0,
-      created_at: '2024-08-27T13:44:16+00:00',
-      score_detail: [
-        {
-          name: '데드리프트',
-          score: 0,
-        },
-        {
-          name: '벤치프레스',
-          score: 0,
-        },
-        {
-          name: '스쿼트',
-          score: 0,
-        },
-      ],
-    },
-  ],
-  user_info: [
-    {
-      nickname: '따잇',
-      profile_image: require('../../assets/images/profile.png'),
-    },
-    {
-      nickname: '나라',
-      profile_image: require('../../assets/images/profile.png'),
-    },
-  ],
-  usage_data: ['데드리프트', '벤치프레스', '스쿼트'],
-};
+/* eslint-disable */
 
 const CompetitionRoom1VS1 = () => {
   const maxGraphWidth = width - 180;
-  const [competitionRecord, setCompetitionRecord] = useState();
   const route = useRoute();
-  const [competitionData, setCompetitionData] = useState();
   const { competitionId } = route.params;
+  const [competitionData, setCompetitionData] = useState();
+  const [competitionRecord, setCompetitionRecord] = useState();
 
   useEffect(() => {
     const fetchCompetitionDetail = async () => {
@@ -110,72 +44,75 @@ const CompetitionRoom1VS1 = () => {
         Alert.alert('경쟁방 상세 정보 조회 실패', error.message);
       }
     };
+
+    const fetchCompetitionRecord = async () => {
+      try {
+        const res = await getCompetitionRecord(competitionId);
+        if (res.status === 200) {
+          const sortedData = res.data[1].is_my_record ? [res.data[1], res.data[0]] : [res.data[0], res.data[1]];
+          setCompetitionRecord(sortedData);
+        }
+      } catch (error) {
+        console.log('error: ', error);
+      }
+    };
+
     fetchCompetitionDetail();
+    fetchCompetitionRecord();
   }, [competitionId]);
 
-  useEffect(() => {
-    setCompetitionRecord(dummy_data.record);
-    // fetchResult();
-  }, []);
-
-  const fetchResult = async () => {
-    try {
-      const res = getCompetitionRecord(competitionId);
-      if (res.status === 200) {
-        // userId 가져오기
-        const userId = 'member_uuid1';
-        const sortedData = res.data[1].member_id === userId ? [res.data[1], res.data[0]] : [res.data[0], res.data[1]];
-        setCompetitionRecord(sortedData);
-      }
-    } catch (error) {
-      console.log('error: ', error);
-    }
-  };
-
-  const CompetitionProfile = ({ userInfo, result, color, style }) => {
+  const CompetitionProfile = ({ record, color, style }) => {
+    console.log(record.member_info);
     return (
       <View style={[styles.profileWrapper, style]}>
-        <Image style={{ width: 80, height: 80 }} source={userInfo.profile_image} />
+        <Image
+          style={{ width: 80, height: 80 }}
+          source={record.member_info.profile_image ? { uri: record.member_info.profile_image } : dummyProfile}
+        />
         <View>
           <View style={styles.profileTextWrapper}>
-            <Text style={styles.profileText}>{userInfo.nickname}</Text>
+            <Text style={styles.profileText}>{record.member_info.nickname}</Text>
             <Text style={styles.profileTailText}>님</Text>
           </View>
-          <Text style={[styles.profileText, { color: color }]}>{result.total_score}</Text>
+          <Text style={[styles.profileText, { color: color }]}>{record.total_score}</Text>
         </View>
       </View>
     );
   };
 
-  const Competition1VS1Header = () => {
+  const Competition1VS1Header = ({ data }) => {
     return (
-      <View>
+      <View style={{ paddingBottom: 20 }}>
         <View style={styles.messageWrapper}>
-          <Text style={styles.userNameText}>{dummy_data.user_info[0].nickname}님,</Text>
+          <Text style={styles.userNameText}>{data[0].member_info.nickname}님,</Text>
           <Text style={styles.messageText}>
-            {competitionRecord[0].total_score >= competitionRecord[1].total_score
-              ? '지금 이기고 있네요! 계속 가봅시다'
-              : '지고있어요.. 조금만 더 힘내요!'}
+            {(() => {
+              if (data[0].total_score > data[1].total_score) {
+                return '지금 이기고 있네요! 계속 가봅시다';
+              } else if (data[0].total_score < data[1].total_score) {
+                return '지고있어요.. 조금만 더 힘내요!';
+              } else {
+                return '비기고 있어요! 조금만 더 해봅시다';
+              }
+            })()}
           </Text>
         </View>
         <View style={styles.profileContainer}>
           <CompetitionProfile
-            userInfo={dummy_data.user_info[0]}
-            result={competitionRecord[0]}
+            record={data[0]}
             color={COLORS.primary}
             style={
-              competitionRecord[0].total_score >= competitionRecord[1].total_score && {
+              data[0].total_score >= data[1].total_score && {
                 borderWidth: 3,
                 borderColor: COLORS.primary,
               }
             }
           />
           <CompetitionProfile
-            userInfo={dummy_data.user_info[1]}
-            result={competitionRecord[1]}
+            record={data[1]}
             color={COLORS.secondary}
             style={
-              competitionRecord[1].total_score >= competitionRecord[0].total_score && {
+              data[1].total_score >= data[0].total_score && {
                 borderWidth: 3,
                 borderColor: COLORS.secondary,
               }
@@ -236,13 +173,13 @@ const CompetitionRoom1VS1 = () => {
   return (
     <SafeAreaView style={styles.pageContainer}>
       {competitionData && <CompetitionRoomHeader data={competitionData} />}
-      {competitionRecord[0] && competitionRecord[1] && (
+      {competitionRecord && (
         <View style={[{ paddingTop: 30 }, LAYOUT_PADDING]}>
           <FlatList
-            data={dummy_data.record[0].score_detail}
-            keyExtractor={(item, index) => index}
+            data={competitionRecord[0].score_detail}
+            keyExtractor={(item, index) => item.name}
             renderItem={renderScoreItem}
-            ListHeaderComponent={Competition1VS1Header}
+            ListHeaderComponent={<Competition1VS1Header data={competitionRecord} />}
             ListFooterComponent={<View style={{ height: 30 }} />}
             showsVerticalScrollIndicator={false}
             contentContainerStyle={{ gap: 10 }}
