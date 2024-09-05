4bd5d755bb49e69dbcb26fcb5275076fa47bea40
ChangHyeon Lee <canghaun@gmail.com>
2024-09-06 03:41:35 +0900

    ISSUE-259 :bug: Fix: 잘못된 주석 제거

--- src/pages/competition/rankingPageTabs/RankList.jsx
+++ src/pages/competition/rankingPageTabs/RankList.jsx
@@ -44,12 +44,15 @@ const RankList = ({ data, competitionData, progress, onJoin, onLeave }) => {
         {data && (
           <View style={{ alignItems: 'center' }}>
             <View style={styles.podiumWrapper}>
-              {/* {progress === 'AFTER' && ( */}
-              <Image
-                style={[styles.spotlightImage, { width: spotlightImageSize.width, height: spotlightImageSize.height }]}
-                source={spotlightImage}
-              />
-              {/* )} */}
+              {progress === 'AFTER' && (
+                <Image
+                  style={[
+                    styles.spotlightImage,
+                    { width: spotlightImageSize.width, height: spotlightImageSize.height },
+                  ]}
+                  source={spotlightImage}
+                />
+              )}
               <Image style={{ width: podiumImageSize.width, height: podiumImageSize.height }} source={podiumImage} />
               {data[0] && (
                 <>
@@ -64,12 +67,12 @@ const RankList = ({ data, competitionData, progress, onJoin, onLeave }) => {
                       data[0].member_info.profile_image ? { uri: data[0].member_info.profile_image } : dummyProfile
                     }
                   />
-                  {/* {progress === 'AFTER' && ( */}
-                  <Image
-                    style={[styles.crownImage, { bottom: 0.85 * podiumImageSize.height + 74 }]}
-                    source={crownImage}
-                  />
-                  {/* )} */}
+                  {progress === 'AFTER' && (
+                    <Image
+                      style={[styles.crownImage, { bottom: 0.85 * podiumImageSize.height + 74 }]}
+                      source={crownImage}
+                    />
+                  )}
                 </>
               )}
               {data[1] && (
