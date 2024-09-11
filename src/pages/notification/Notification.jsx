2d5db9a78364bc650c0535e2377c342f00a50bd5
ChangHyeon Lee <canghaun@gmail.com>
2024-09-11 21:46:44 +0900

    ISSUE-290 :sparkles: Feat: Notification 페이지 틀잡기

---
+++ src/pages/notification/Notification.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+const Notification = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Notification</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Notification;
+
+const styles = StyleSheet.create({});
