186e5bb5c6e238b5ad648cc244e5dc8ef979a78a
nara04040 <nara04040@naver.com>
2024-09-23 21:00:20 +0900

    ISSUE-299 :sparkles: Feat: 세트수 렌더링 방식 수정

--- ios/Ddait/AppDelegate.mm
+++ ios/Ddait/AppDelegate.mm
@@ -2,6 +2,8 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <Firebase.h>
+
 
 @implementation AppDelegate
 
@@ -11,6 +13,8 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+  [FIRApp configure];
+
   
   BOOL success = [super application:application didFinishLaunchingWithOptions:launchOptions];
    if (success) {
